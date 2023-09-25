94139d9ae24371b5b203d932e3f5bfe9efebf35a
Daniele Parisi <neb.inf@gmail.com>
2023-09-25 16:00:37 +0200

    Add properties for externalized configuration on Consul

--- src/test/java/com/danservice/techstarter/ApplicationContextTest.java
+++ src/test/java/com/danservice/techstarter/ApplicationContextTest.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Import;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 import static com.danservice.techstarter.DemoApplicationTests.ApplicationTestConfiguration;
 
+@EmbeddedKafka
 @SpringBootTest
 @Import(ApplicationTestConfiguration.class)
 class DemoApplicationTests {
